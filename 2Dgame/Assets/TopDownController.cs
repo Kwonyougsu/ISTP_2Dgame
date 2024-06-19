e1a856afab17b281cb8031c2dda584615aad81ba
Kwonyougsu <ysun5311@gmail.com>
2024-06-19 16:01:55 +0900

    Update : 캐릭터 이동구현

---
+++ 2Dgame/Assets/TopDownController.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+
+
+public class TopDownController
+ : MonoBehaviour
+{
+    public event Action<Vector2> OnMoveEvent;
+    public event Action<Vector2> OnLookEvent;
+
+    public void CallMoveEvent(Vector2 direction)
+    {
+        //?.없으면 말고 있으면 실행
+        OnMoveEvent?.Invoke(direction);
+    }
+    public void CallLookEvent(Vector2 direction)
+    {
+        OnLookEvent?.Invoke(direction);
+    }
+}
---
+++ 2Dgame/Assets/TopDownMovement.cs
@@ -0,0 +1,50 @@
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class TopDownMovement : MonoBehaviour
+{
+    private TopDownController controller;
+    private Rigidbody2D movementRigidbody;
+    private Vector2 movementDirection;
+
+    [SerializeField] private int speed = 5;
+    [SerializeField] private SpriteRenderer characterRenderer;
+
+    private void Awake()
+    {
+        controller = GetComponent<TopDownController>();
+        movementRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        controller.OnMoveEvent += Move;
+    }
+    private void Move(Vector2 dire)
+    {
+
+        //¿À¸¥ÂÊ 1 => false, ¿ÞÂÊ -1 =>true
+        movementDirection = dire;
+        if(dire.x > 0)
+        {
+            characterRenderer.flipX = false;
+        }
+        if(dire.x < 0)
+        {
+            characterRenderer.flipX = true;
+        }
+        
+    }
+    private void FixedUpdate()
+    {
+        AppiyMovemant(movementDirection);
+    }
+    private void AppiyMovemant(Vector2 dire)
+    {
+        dire = dire * speed;
+        movementRigidbody.velocity = dire;
+    }
+
+
+}
+

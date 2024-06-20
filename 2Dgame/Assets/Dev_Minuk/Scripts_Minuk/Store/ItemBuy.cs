c17e92bd46595168d5eda5c0c3a3e97e6adc190c
Kwonyougsu <ysun5311@gmail.com>
2024-06-20 15:01:52 +0900

    fix : 충돌 수정

--- 2Dgame/Assets/Dev_Minuk/Scripts_Minuk/Store/ItemBuy.cs
+++ 2Dgame/Assets/Dev_Minuk/Scripts_Minuk/Store/ItemBuy.cs
@@ -8,7 +8,6 @@ using UnityEngine.UI;
 public class ItemBuy : MonoBehaviour
 {
     public ItemData itemData;
-    //public PlayerStatsHandler playerStatsHandler;
 
     public void OnBuyButton()
     {
@@ -17,36 +16,7 @@ public class ItemBuy : MonoBehaviour
             // 소지금액에서 itemData.itemprice[UIStore.instance.curIndex] 빼주기
             if (itemData.itemstack[UIStore.instance.curIndex] < 3)
             itemData.itemstack[UIStore.instance.curIndex]++;
-          //ApplyItemStats(itemData.itemstack[UIStore.instance.curIndex]);
         }
         UIStore.instance.SetStotore();
     }    
-//    private void ApplyItemStats(int itemIndex)
-//    {
-//        PlayerStats itemStats = new PlayerStats();
-
-//       // itemIndex에 따라 증가시킬 스탯을 설정
-//        switch (itemIndex)
-//        {
-//            case 0: // AttackPowerUp
-//                itemStats.statsChangeType = StatsChangeType.Add;
-//                itemStats.attackSO = new AttackSO { power = 5 };
-//break;
-//            case 1: // MoveSpeedUp
-//    itemStats.statsChangeType = StatsChangeType.Add;
-//    itemStats.speed = 3f;
-//    break;
-//case 2: // HPMaxUp
-//    itemStats.statsChangeType = StatsChangeType.Add;
-//    itemStats.maxHealth = 10;
-//    break;
-//case 3: // BulletUp
-//    itemStats.statsChangeType = StatsChangeType.Add;
-//    itemStats.attackSO = new AttackSO { speed = 0.1f };
-//    break;
-//}
-
-//        playerStatsHandler.AddStatModifier(itemStats);
-//    }
-
 }

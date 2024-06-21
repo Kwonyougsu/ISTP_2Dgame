cf9f144b97c9ad3dc6d01db87a331de382ecdd14
Github <Github>
2024-06-21 12:43:13 +0900

    Fix DropItem
    DropItem이 인식할
    Player Component 변경

--- 2Dgame/Assets/Dev_Minuk/Scripts_Minuk/DropItem/DropItemCoin.cs
+++ 2Dgame/Assets/Dev_Minuk/Scripts_Minuk/DropItem/DropItemCoin.cs
@@ -6,7 +6,7 @@ public class DropItemCoin : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.GetComponent<PlayerStats>() != null) //플레이어만 가지고 있는 컴포넌트
+        if (collision.GetComponent<PlayerInputController>() != null) //플레이어만 가지고 있는 컴포넌트
         {
             GameManager.Instance.stageGold += 100;
             Destroy(gameObject);

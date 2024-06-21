d7e949e50642bd485406adcaedb8feb56e4431e7
leemoojin <hzm_deli@naver.com>
2024-06-21 12:32:05 +0900

    feat: spawn

--- 2Dgame/Assets/Dev_MJ/Scripts/Enemy/Controllers/ContactEnemyController.cs
+++ 2Dgame/Assets/Dev_MJ/Scripts/Enemy/Controllers/ContactEnemyController.cs
@@ -34,10 +34,8 @@ public class ContactEnemyController : EnemyController
     {
         Vector2 direction = Vector2.zero;
 
-        // 타겟 추격 가능
         if (DistanceToTarget() < followRange)
         {
-            // 타겟 방향
             direction = DirectionToTarget();
         }
         
@@ -52,7 +50,6 @@ public class ContactEnemyController : EnemyController
 
     private void Rotate(Vector2 direction)
     {        
-        // Atan2는 가로와 세로의 비율을 바탕으로 -파이~파이(-180도~180도에 대응, * Rad2Deg가 그 기능)하는 값을 나타내주는 함수였다는 것 기억하시죠?
         float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         //characterRenderer.flipX = Mathf.Abs(rotZ) > 90f;//나중에 이미지 추가할것**
     }

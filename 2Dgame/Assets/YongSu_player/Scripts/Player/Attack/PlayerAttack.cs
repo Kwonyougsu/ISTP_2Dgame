b5eea0f83acbbe92bb71e26697023609368235f1
Kwonyougsu <ysun5311@gmail.com>
2024-06-24 23:30:59 +0900

    Update: 레벨업 시 원거리 공격 갯수 추가

--- 2Dgame/Assets/YongSu_player/Scripts/Player/Attack/PlayerAttack.cs
+++ 2Dgame/Assets/YongSu_player/Scripts/Player/Attack/PlayerAttack.cs
@@ -8,7 +8,7 @@ public class PlayerAttack : MonoBehaviour
     private SpriteRenderer characterRenderer;
     private GameObject player;
 
-    public float RangedAttackspeed = 100f;
+    public float RangedAttackspeed = 50f;
     public float detectionRange = 50f;
     public Sprite[] characer;
     private int playerid;
@@ -50,7 +50,7 @@ public class PlayerAttack : MonoBehaviour
     {
         if (playerid == 2) // 계속 돌아가야하므로, Update
         {
-            RotationAttack(GameManager.Instance.Lv+1);
+            RotationAttack(GameManager.Instance.Lv + 1);
         }
     }
 
@@ -61,12 +61,17 @@ public class PlayerAttack : MonoBehaviour
         {
             if (id == 0)
             {
-                closeAttack(GameManager.Instance.Lv+1);
+                closeAttack(GameManager.Instance.Lv + 1);
                 yield return new WaitForSeconds(0.01f);
+                if (GameManager.Instance.Lv >= 2)
+                {
+                    Detected(GameManager.Instance.Lv + 1);
+                    yield return new WaitForSeconds(0.01f);
+                }
             }
             else if (id == 1) //원거리는 탐지 먼저 해야함
             {
-                Detected();
+                Detected(GameManager.Instance.Lv + 1);
                 yield return new WaitForSeconds(0.01f);
             }
             yield return new WaitForSeconds(1f - (GameManager.Instance.upgradeStatData.statLv[2] * 0.05f));
@@ -157,22 +162,38 @@ public class PlayerAttack : MonoBehaviour
 
     #region 원거리 공격
 
-    private void Detected() // 원거리 적 감지
+    private void Detected(int CountLv) // 원거리 적 감지
     {
-        Collider2D[] hitColliders = Physics2D.OverlapCircleAll(player.transform.position, detectionRange);
-        foreach (var hitCollider in hitColliders)
+        if (CountLv >= 0 && CountLv < 2)
         {
+            Collider2D hitCollider = Physics2D.OverlapCircle(player.transform.position, detectionRange);
             if (hitCollider.CompareTag("Enemy"))
             {
                 RangedAttack(hitCollider.transform);
             }
+        }
+        else if (CountLv >= 2)
+        {
+            Collider2D[] hitColliders = Physics2D.OverlapCircleAll(player.transform.position, detectionRange);
+
+            int maxColliders = 2;
+            int colliderCount = Mathf.Min(hitColliders.Length, maxColliders);
+
+            for (int i = 0; i < colliderCount; i++)
+            {
+                if (hitColliders[i].CompareTag("Enemy"))
+                {
+                    RangedAttack(hitColliders[i].transform);
+                }
+            }
+
         }
     }
     private void RangedAttack(Transform target) // 원거리 공격 
     {
         GameObject Attack = Instantiate(AttackPrefab[1]);
         float time = 2; // 파괴시간
-     
+
         Vector3 direction = (target.position - player.transform.position).normalized;
         Attack.GetComponent<Rigidbody2D>().velocity = direction * RangedAttackspeed;
 
@@ -190,7 +211,7 @@ public class PlayerAttack : MonoBehaviour
             Attack.transform.position = new Vector3(player.transform.position.x, player.transform.position.y);
         }
 
-        Destroy(Attack,time);
+        Destroy(Attack, time);
     }
     #endregion
 
@@ -198,7 +219,7 @@ public class PlayerAttack : MonoBehaviour
     public void RotationAttack(int objsizeLv)
     {
         deg = deg % 360;
-        if(objsizeLv >= 3) objsizeLv = 3;
+        if (objsizeLv >= 3) objsizeLv = 3;
 
         if (objsizeLv > 0 && objsizeLv < 2) //한개 소환
         {
@@ -222,10 +243,10 @@ public class PlayerAttack : MonoBehaviour
                 Destroy(RotationAttackInstance);
             }
 
-            if (mutipleRotationAttackInstance != null && mutipleRotationAttackInstance.Length == objsizeLv-1)
+            if (mutipleRotationAttackInstance != null && mutipleRotationAttackInstance.Length == objsizeLv - 1)
             {
                 for (int i = 0; i < mutipleRotationAttackInstance.Length; i++)
-                Destroy(mutipleRotationAttackInstance[i]);
+                    Destroy(mutipleRotationAttackInstance[i]);
             }
 
             if (mutipleRotationAttackInstance == null || mutipleRotationAttackInstance.Length != objsizeLv)

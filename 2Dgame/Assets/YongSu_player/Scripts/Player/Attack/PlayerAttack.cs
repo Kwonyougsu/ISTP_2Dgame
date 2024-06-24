09153c779cb14d6d279edf498be56e48206c01e7
Kwonyougsu <ysun5311@gmail.com>
2024-06-24 23:13:29 +0900

    Update : 레벨업 시 근거리 공격 추가

--- 2Dgame/Assets/YongSu_player/Scripts/Player/Attack/PlayerAttack.cs
+++ 2Dgame/Assets/YongSu_player/Scripts/Player/Attack/PlayerAttack.cs
@@ -15,7 +15,8 @@ public class PlayerAttack : MonoBehaviour
     private GameObject closeAttackInstance;
     private GameObject RotationAttackInstance;
     public GameObject[] mutipleRotationAttackInstance;
- 
+    public GameObject[] mutiplecloseAttackInstance;
+
     float circleR;
     float deg;
     float objSpeed;
@@ -60,7 +61,7 @@ public class PlayerAttack : MonoBehaviour
         {
             if (id == 0)
             {
-                closeAttack();
+                closeAttack(GameManager.Instance.Lv+1);
                 yield return new WaitForSeconds(0.01f);
             }
             else if (id == 1) //원거리는 탐지 먼저 해야함
@@ -73,33 +74,84 @@ public class PlayerAttack : MonoBehaviour
     }
 
     #region 근거리 공격
-    private void closeAttack()
+    private void closeAttack(int CountLv)
     {
-        if (closeAttackInstance == null)
-        {
-            closeAttackInstance = Instantiate(AttackPrefab[0]);
-            closeAttackInstance.SetActive(false);
-        }
+        if (CountLv >= 2) CountLv = 2;
 
-        closeAttackInstance.SetActive(true);
-        //오른쪽 1 => false, 왼쪽 -1 =>true
-        if (!characterRenderer.flipX)
+        if (CountLv >= 0 && CountLv < 2)
         {
-            closeAttackInstance.transform.position = new Vector3(player.transform.position.x + 1.5f, player.transform.position.y);
-            closeAttackInstance.GetComponent<SpriteRenderer>().flipY = false;
+            if (closeAttackInstance == null)
+            {
+                closeAttackInstance = Instantiate(AttackPrefab[0]);
+                closeAttackInstance.SetActive(false);
+            }
+            closeAttackInstance.SetActive(true);
+
+            if (!characterRenderer.flipX)
+            {
+                closeAttackInstance.transform.position = new Vector3(player.transform.position.x + 1.5f, player.transform.position.y);
+                closeAttackInstance.GetComponent<SpriteRenderer>().flipY = false;
+            }
+            else
+            {
+                closeAttackInstance.transform.position = new Vector3(player.transform.position.x - 1.5f, player.transform.position.y);
+                closeAttackInstance.GetComponent<SpriteRenderer>().flipY = true;
+            }
+            StartCoroutine(EndcloseAttack(closeAttackInstance, 0.1f)); //공격 유지시간
         }
-        else
+        else if (CountLv >= 2)
         {
-            closeAttackInstance.transform.position = new Vector3(player.transform.position.x - 1.5f, player.transform.position.y);
-            closeAttackInstance.GetComponent<SpriteRenderer>().flipY = true;
+            if (closeAttackInstance != null)
+            {
+                Destroy(closeAttackInstance);
+            }
+
+            if (mutiplecloseAttackInstance == null || mutiplecloseAttackInstance.Length != CountLv)
+            {
+                mutiplecloseAttackInstance = new GameObject[CountLv];
+                for (int i = 0; i < CountLv; i++)
+                {
+                    mutiplecloseAttackInstance[i] = Instantiate(AttackPrefab[0]);
+                    mutiplecloseAttackInstance[i].SetActive(false);
+                }
+            }
+
+            for (int i = 0; i < CountLv; i++)
+            {
+                mutiplecloseAttackInstance[i].SetActive(true);
+                if (!characterRenderer.flipX)
+                {
+                    mutiplecloseAttackInstance[0].transform.position = new Vector3(player.transform.position.x + 1.5f, player.transform.position.y);
+                    mutiplecloseAttackInstance[0].GetComponent<SpriteRenderer>().flipY = false;
+                    mutiplecloseAttackInstance[1].transform.position = new Vector3(player.transform.position.x - 1.5f, player.transform.position.y);
+                    mutiplecloseAttackInstance[1].GetComponent<SpriteRenderer>().flipY = true;
+                }
+                else
+                {
+                    mutiplecloseAttackInstance[0].transform.position = new Vector3(player.transform.position.x + 1.5f, player.transform.position.y);
+                    mutiplecloseAttackInstance[0].GetComponent<SpriteRenderer>().flipY = false;
+                    mutiplecloseAttackInstance[1].transform.position = new Vector3(player.transform.position.x - 1.5f, player.transform.position.y);
+                    mutiplecloseAttackInstance[1].GetComponent<SpriteRenderer>().flipY = true;
+                }
+
+            }
+            StartCoroutine(EndmuticloseAttack(mutiplecloseAttackInstance, 0.1f)); //공격 유지시간
         }
-        StartCoroutine(EndAttack(closeAttackInstance, 0.1f)); //공격 유지시간
     }
 
-    IEnumerator EndAttack(GameObject Attack, float delay)
+    IEnumerator EndcloseAttack(GameObject Attack, float delay)
     {
         yield return new WaitForSeconds(delay);
         Attack.SetActive(false);
+    }
+
+    IEnumerator EndmuticloseAttack(GameObject[] Attacks, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        foreach (var attack in Attacks)
+        {
+            attack.SetActive(false);
+        }
     }
     #endregion
 
@@ -148,7 +200,7 @@ public class PlayerAttack : MonoBehaviour
         deg = deg % 360;
         if(objsizeLv >= 3) objsizeLv = 3;
 
-        if (objsizeLv > 0 && objsizeLv < 2)
+        if (objsizeLv > 0 && objsizeLv < 2) //한개 소환
         {
             if (RotationAttackInstance == null)
             {
@@ -162,9 +214,8 @@ public class PlayerAttack : MonoBehaviour
             var y = circleR * Mathf.Cos(rad);
             RotationAttackInstance.transform.position = transform.position + new Vector3(x, y);
             RotationAttackInstance.transform.rotation = Quaternion.Euler(0, 0, deg * -1); // 가운데를 바라보게 각도 조절
-            Debug.Log(RotationAttackInstance.transform.position);
         }
-        else if (objsizeLv >= 2)
+        else if (objsizeLv >= 2) // 두개 이상 but 3개가 최대 
         {
             if (RotationAttackInstance != null)
             {
@@ -199,7 +250,6 @@ public class PlayerAttack : MonoBehaviour
                 var y = circleR * Mathf.Cos(rad);
                 mutipleRotationAttackInstance[i].transform.position = transform.position + new Vector3(x, y);
                 mutipleRotationAttackInstance[i].transform.rotation = Quaternion.Euler(0, 0, (deg + (i * (360 / objsizeLv))) * -1);
-                Debug.Log(mutipleRotationAttackInstance[i].transform.position);
             }
         }
     }
